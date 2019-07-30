0e09efa8e29ad2f6324fb32be5b9f11f1f96c9c3
rajasd27 <rajasdeshpande27@gmail.com>
2019-07-30 15:21:52 -0500

    Asset created by rajasd27

---
+++ portals/TrainingPortal/config/widgets/HTML_WIDGET_COMPONENT_c32a0c18-ab8e-4bf6-b55b-7e68b7bf2e2a/parsers/html/incoming_parser/index.js
@@ -0,0 +1,13 @@
+var buttonGroupClick = function(event){
+    // $(event.target.parentElement).attr("selection",event.target.value);
+    // $(event.target.parentElement).children().removeClass("btn-outline-primary");
+    // $(event.target.parentElement).children().addClass("btn-secondary")
+    // $(event.target).removeClass("btn-secondary")
+    // $(event.target).addClass("btn-outline-primary")
+    CB_PORTAL.selectPage(event.target.value);
+}
+
+$(".headerNavButton").click(function(event){
+    buttonGroupClick(event)
+});
+
