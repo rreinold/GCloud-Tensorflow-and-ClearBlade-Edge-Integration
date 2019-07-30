ce48cf92573bfd6bbe16a6c79b8f00f95bf0b3fa
rajasd27 <rajasdeshpande27@gmail.com>
2019-07-30 15:22:22 -0500

    Asset created by rajasd27

---
+++ portals/TrainingPortal/config/widgets/TEXT_WIDGET_COMPONENT_bc7ed252-906f-47e9-b6dd-b82571ad0875/parsers/textContent/incoming_parser/index.js
@@ -0,0 +1,6 @@
+/**
+* @param {context} ctx - Object containing data and model information for this item.
+*/
+parser = (ctx) => {
+  return "Click on the column names below to convert Non Categorical data to Numeric Data"
+}
