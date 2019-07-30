6db7b1a37368ab8364cdf855c7180afdea858184
rajasd27 <rajasdeshpande27@gmail.com>
2019-07-30 15:22:09 -0500

    Asset created by rajasd27

---
+++ portals/TrainingPortal/config/widgets/TEXT_WIDGET_COMPONENT_081ecb45-0dfd-4ecb-ad58-6ce6963135d2/parsers/textContent/incoming_parser/index.js
@@ -0,0 +1,6 @@
+/**
+* @param {context} ctx - Object containing data and model information for this item.
+*/
+parser = (ctx) => {
+  return "Click on the Features column to select the features for Training the Model"
+}
